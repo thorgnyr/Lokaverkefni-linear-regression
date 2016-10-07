cc073958bf52c70452916e070c03baee3b7efb67
thorgnyr <thorgnyr@lanti.cc>
2016-10-07 21:02:46 +0000

    Final að undanskyldu testmodel keyrslu

--- v5.R
+++ v5.R
@@ -34,19 +34,16 @@ testdata <- fasteignir_sorted[third,]
 traindata <- fasteignir_sorted[-third,]
 
 #################HINGAÐ ER ALLT EINS###########################
-
 #Gerum módel og steppum það.----
 stort_model <- lm(nuvirdi ~ ., data=traindata)
 stort.st <- step(stort_model)
 summary(stort.st)
 
 #Skerum út jaðarmælingar úr stóra módelinu.----
-plot(stort.st)
 diag1 <- fortify(stort.st)
 diag1 <- diag1[diag1$.stdresid < 6.5, ]
 diag1.m <- lm(nuvirdi ~ teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjklos +fjstof + matssvaedi + ibteg + kaup_ar, data = diag1)
 summary(diag1.m)
-plot(diag1.m)
 
 #Skipta byggingarári í þrjár breytur og transform----
 diag1$byggarf <- cut(diag1$byggar,c(0,1938,1960,1983, 2016), right=F)
@@ -56,29 +53,8 @@ diag1.byggar <- lm(sqrt(nuvirdi) ~ teg_eign + byggarf + haednr + lyfta + ibm2 + 
 summary(diag1.byggar)
 plot(diag1.byggar)
 
+#Keyra þetta á testmódelið.----
+thefinalcountdown <- predict(diag1.byggar, newdata = testdata)
 
-#Prufa að búa til módel úr kaupárum á núvirði.----
 
 
-#Athugasemdir-------
-
-#Prufukóðar---------
-m1<-lm(nuvirdi ~ ., na.action = "na.exclude", data = train)
-sum(is.na(residuals(m1)))
-sm1<-summary(m1)
-sm1
-####diagnostic
-m2<-lm(nuvirdi~teg_eign+byggar+efnu+ibm2+fjherb, train)
-#skoða þætti sem líklegir eru til að hafa áhrif á verð 
-#sm2<-summary(lm(nuvirdi~kaupverd+grfast+fjmib+fjhaed, train))
-sm2<-summary(m2)
-m3<-update(m1, .~.-faerslunumer)
-m4<-update(m3, .~. -rfastnum)
-#m5<-update(m4, .~. -kaupverd)
-m6<-update(m4, .~. -svfn)
-m7<-update(m6, .~. -byggd)
-m8<-update(m7, .~. -adferd)
-m9<-update(m8, .~. -ib3m2)
-
-lm_herb_serherb <- lm(fjherb ~ I(fjbkar + fjsturt + fjklos + fjeld + fjstof), data = trainmodel)
-summary(lm_herb_serherb)
