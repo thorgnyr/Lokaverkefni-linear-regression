15d657f8fbbf6df9b81d1d9a34bdeca423cde381
thorgnyr <thorgnyr@lanti.cc>
2016-10-10 17:03:53 +0000

    Vandræðagangur í lokin

--- v5.R
+++ v5.R
@@ -51,38 +51,43 @@ levels(diag1$byggar)<-c("gamalt","midlungs", "lanabreyting", "nytt")
 diag1.byggar <- lm(sqrt(nuvirdi) ~ teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjklos +fjstof + matssvaedi + ibteg + kaup_ar, data = diag1)
 summary(diag1.byggar)
 
-#Keyra þetta á testmódelið.----
-thefinalcountdown <- predict(diag1.byggar, newdata = testdata)
 
+#########TESTMODEL VINNA###############
 
-
-
-
-
-
-
-
-
-
-
-#Gerum módel og steppum það.----
+#Gerum módel og steppum það.
 litid_model <- lm(nuvirdi ~ ., data=testdata)
+extr.byggar <- as.data.frame(testdata$byggar)
 litid.st <- step(litid_model)
 summary(litid_model)
 
-#Skerum út jaðarmælingar úr stóra módelinu.----
+#Skerum út jaðarmælingar úr stóra módelinu.
 diag2 <- fortify(litid.st)
+diag2 <- cbind(diag2, extr.byggar)
 diag2 <- diag2[diag2$.stdresid < 6.5, ]
-diag2.m <- lm(nuvirdi ~ teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjklos +fjstof + matssvaedi + ibteg + kaup_ar, data = diag2)
+diag2.m <- lm(nuvirdi ~ teg_eign + `testdata$byggar` + haednr + lyfta + ibm2 + fjhaed + fjklos +fjstof + matssvaedi + ibteg + kaup_ar, data = diag2)
 summary(diag2.m)
 
-#Skipta byggingarári í þrjár breytur og transform----
-diag2$byggarf <- cut(diag2$byggar,c(0,1938,1960,1983, 2016), right=F)
-levels(diag2$byggarf)<-c("gamalt","midlungs", "lanabreyting", "nytt")
-diag2 <- diag2[-3]
-diag2.byggar <- lm(sqrt(nuvirdi) ~ teg_eign + byggarf + haednr + lyfta + ibm2 + fjhaed + fjklos +fjstof + matssvaedi + ibteg + kaup_ar, data = diag2)
+#Skipta byggingarári í þrjár breytur og transform
+diag2$`testdata$byggar` <- cut(diag2$`testdata$byggar`,c(0,1938,1960,1983, 2016), right=F)
+levels(diag2$`testdata$byggar`)<-c("gamalt","midlungs", "lanabreyting", "nytt")
+diag2.byggar <- lm(sqrt(nuvirdi) ~ teg_eign + `testdata$byggar` + haednr + lyfta + ibm2 + fjhaed + fjklos +fjstof + matssvaedi + ibteg + kaup_ar, data = diag2)
 summary(diag2.byggar)
 plot(diag2.byggar)
 
+#Þetta----
+
+litid_model <- lm(nuvirdi ~ ., data=testdata)
+extr.byggar <- as.data.frame(testdata$byggar)
+litid.st <- step(litid_model)
+diag2 <- fortify(litid.st)
+diag2 <- cbind(diag2, extr.byggar)
+diag2$byggar <- diag2$`testdata$byggar`
+diag2 <- diag2[diag2$.stdresid < 6.5, ]
+diag2$byggar <- cut(diag2$byggar, c(0,1938,1960,1983, 2016), right=F)
+levels(diag2$byggar)<-c("gamalt","midlungs", "lanabreyting", "nytt")
 
 
+
+
+#Keyra þetta á testmódelið.----
+thefinalcountdown <- predict(diag1.byggar, newdata = diag2)
